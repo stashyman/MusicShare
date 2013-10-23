be99f005b6f6a45cfcce972c3cd9fcbeabfcb0fc
Terryben <Ben18181820@gmail.com>
2013-10-23 15:56:04 -0600

    Update User.java

--- User.java
+++ User.java
@@ -5,6 +5,7 @@ public class User {
 	//Users only have username, password, and friends list. Do we need any more?
 	private String username;
 	private String password;
+	private LinkedList<Message> messages = new LinkedList<Message>();
 	public LinkedList<Song> ownedLibrary = new LinkedList<Song>();
 	public LinkedList<Song> playableLibrary = new LinkedList<Song>();
 	public LinkedList<Song> borrowedLibrary = new LinkedList<Song>();
@@ -19,6 +20,15 @@ public class User {
 	public User(String username_, String password_){
 		username = username_;
 		password = password_;
+	}
+	public User(User _user){//copy constructor
+		username = _user.getUsername();
+		password = _user.getPassword();
+		friends = new LinkedList<String>();
+		ListIterator LI = _user.getFriends().listIterator();
+		while(LI.hasNext()){
+			friends.add((String)LI.next());
+		}
 	}
 	//Need to add method if friends list is empty so it does not through a no such element exception
 	public void PrintFriends(User u) {
@@ -40,9 +50,15 @@ public class User {
 	public void addFriends(String s) {
 		friends.add(s);
 	}
+	public void addMessage(Message m){
+		messages.add(m);
+	}
 	public String getUsername() {
 		return username;
 		
+	}
+	public String getPassword(){//this may not be what we want but I needed a fast copy constructor
+		return password;
 	}
 	public void addBorrowedSongs(Song s) {
 		borrowedLibrary.add(s);
@@ -53,4 +69,14 @@ public class User {
 	public LinkedList<String> getFriends() {
 		return friends;
 	}
+	public void setPassword(String nPass){
+		password = nPass;
+	}
+	public LinkedList<Message> getMessages(){
+		return messages;
+	}
+	public Message getFirstMessage(){
+		return messages.removeFirst();
+	}
+	
 }
