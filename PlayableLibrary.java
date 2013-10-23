95a576ff89977b56ed64ee19019810fb5abe4dd7
brendonious8 <bjfinnegan@comcast.net>
2013-10-23 13:47:43 -0600

    Create PlayableLibrary.java
    All This does is extend Library so far.

---
+++ PlayableLibrary.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+
+
+public class PlayableLibrary extends Library {
+
+	public PlayableLibrary(User s, LinkedList<Song> lib) {
+		super(s, lib);
+		// TODO Auto-generated constructor stub
+	}
+
+}
