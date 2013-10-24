fff43db8bf44f52c7809e5d8c55b84bb4f5db132
Terryben <Ben18181820@gmail.com>
2013-10-24 10:32:08 -0600

    Update UI.java
    Added an 8th case for testing

--- UI.java
+++ UI.java
@@ -1,14 +1,22 @@
 import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
 
 public class UI {
-	public static void main(String args[]){
+private UserManager UM;
+	public UI(LinkedList<User> Users){
+		UM = new UserManager(Users);
+	}
+	
+	public void runOutput(){
 
 		String a = "";
 		while (a != "3") {
 			System.out
 			.println("\n1. Listen to song/playlist\n\n2. add playlist/song or edit song/playlist\n\n3. borrow/take back song\n\n"
 					+ "4. Profile settings\n\n5. check messages or send a friend request\n\n"
-					+ "6. browse for songs\n\n7. log out\n\nEnter one of the numbers above.\n");
+					+ "6. browse for songs\n\n7. log out\n\n8. Admin/Testing\n\nEnter one of the numbers above.\n");
 			Scanner reader = new Scanner(System.in);
 			String s;
 			s = reader.next();
@@ -221,6 +229,55 @@ public class UI {
 			case "7":{
 				reader.close();
 				return;
+			}
+			case "8":{
+				String b = "";
+				
+				while (b.equals("4") == false) {
+					System.out
+					.println("\n1. print users and passwords\n\n2. Enter a user and view their owned songs. \n\n3. Add a user \n\n4. go back\n\nEnter one of the numbers above.\n");
+					b = reader.next();
+					switch (b) {
+							case "1": {
+								LinkedList<User> ll = UM.getUsers();
+								ListIterator it = ll.listIterator();
+								while(it.hasNext()){
+									User temp = (User)it.next();
+									System.out.println("Name: " + temp.getUsername() + ", " + "Password: " + temp.getPassword());
+								}
+								break;
+							}
+							case "2": {
+								System.out.println("Enter the name of a user.");
+								String _user = reader.next();
+								if(!UM.setCurrentUser(_user)){
+									System.out.print("Name given was not a valid user.");
+									break;
+								}
+								LinkedList<Song> ll = UM.getOwnedSongs();
+								ListIterator it = ll.listIterator();
+								while(it.hasNext()){	
+									System.out.println(it.next().toString());
+								}
+								break;
+							}
+							case "3": {
+								System.out.println("Enter the name of the new user.");
+								String name = reader.next();
+								System.out.println("Enter " + name + "'s password.");
+								String password = reader.next();
+								UM.addUser(name, password);
+								break;
+							}
+							case "4": {
+								//go back
+								break;
+							}
+							default: {
+								System.out
+								.println("Invalid input. Please enter one of the numerical values above.");
+								break;
+							}
 			
 			}
 			
@@ -230,3 +287,4 @@ public class UI {
 	}
 
 }
+}}
