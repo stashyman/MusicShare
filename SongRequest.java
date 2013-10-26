b2137a2faddc0481188286251851e4b68dc358bf
brendonious8 <bjfinnegan@comcast.net>
2013-10-26 16:01:21 -0600

    Update SongRequest.java

--- SongRequest.java
+++ SongRequest.java
@@ -1,19 +1,23 @@
 
 public class SongRequest extends Message{
-	private User sender;
-	private User recipiant;
-	private String message;
-	public SongRequest(User sender_, User recipiant_, String message_) {
-		super(sender_, recipiant_, message_);
+	public SongRequest(User sender_, User recipiant_, String message_, String type_) {
+		super(sender_, recipiant_, message_, type_);
 		// TODO Auto-generated constructor stub
 	}
 	//add the song to the borrowed library
 	public boolean Accepted(Song e) {
-		recipiant.borrowedLibrary.add(e);
+		recipiant.playableLibrary.getSongs().add(e);
 		return true;
 	}
 	public boolean Denied() {
 		return false;
 	}
-
+	public String getType() {
+		return type;
+	}
+	public String toString() {
+		return "From: "+ sender.getUsername() + 
+				"\nTo: "+ recipiant.getUsername() + "\n"+
+				message;
+	}
 }
