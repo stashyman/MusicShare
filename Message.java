20bdea129a27afe0d941bce9aa82e6caa9dbf363
Terryben <Ben18181820@gmail.com>
2013-10-26 11:55:54 -0600

    Update Message.java

--- Message.java
+++ Message.java
@@ -1,8 +1,7 @@
-
 public class Message {
-	private User sender;
-	private User recipiant;
-	private String message;
+	protected User sender;
+	protected User recipiant;
+	protected String message;
 	public Message(User sender_, User recipiant_, String message_) {
 		sender = sender_;
 		recipiant = recipiant_;
