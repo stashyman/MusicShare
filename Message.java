2aa6eecf5e4018dfb6693e59b8051de87671e2e9
brendonious8 <bjfinnegan@comcast.net>
2013-10-28 14:33:50 -0600

    Update Message.java

--- Message.java
+++ Message.java
@@ -4,6 +4,7 @@ public class Message {
 	protected User recipiant;
 	protected String message;
 	protected String type;
+	protected String songname;
 	public Message(User sender_, User recipiant_, String message_, String type_) {
 		sender = sender_;
 		recipiant = recipiant_;
