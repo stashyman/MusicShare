1f10e379e5ac678cde9413c4220a39a1b1586278
Terryben <Ben18181820@gmail.com>
2013-10-30 15:07:57 -0600

    It sorts
    I gave up on mergeSort.  Too complicated with java iterators.  This is n2 instead of nlog(n) but I needed to give you guys something.

--- Library.java
+++ Library.java
@@ -1,14 +1,17 @@
 import java.util.LinkedList;
 import java.util.ListIterator;
+import java.util.Collections;
+import java.util.Comparator;
 
 
 public class Library {
 	//Library only has the Owner of the Library and the List of the Songs that the user owns.
 	private User Owner;
 	private LinkedList<Song> Library = new LinkedList<Song>();
+	private Sort sortedBy;
 
 	public Library(){
-		
+
 	}
 	public Library(User s, LinkedList<Song> lib) {
 		Owner = s;
@@ -20,13 +23,119 @@ public class Library {
 	public LinkedList<Song> getSongs(){
 		return Library;
 	}
-	
-	public void sortLib(LinkedList ll, Sort _sort){
-		LinkedList sorted = new LinkedList();
-		ListIterator li = sorted.listIterator();
-		
+	public void setSortBy(Sort _sort){
+		sortedBy = _sort;
 	}
+	public void sortLib(){
+		LinkedList<Song> temp = new LinkedList<Song>();
+		ListIterator<Song> libIt = Library.listIterator();
 
-	
-	
+		//while there are elements in the library
+		while(libIt.hasNext()){
+			boolean insert = false;
+			//reset the new library iterator
+			ListIterator<Song> tempIt = temp.listIterator();
+			//if the library is empty just add the element
+			if(!tempIt.hasNext()){
+				temp.add(libIt.next());
+				insert = true;
+			}
+			else{
+				while(tempIt.hasNext() && insert == false){
+					switch(sortedBy){
+					case title:{
+						if(libIt.next().getName().compareTo(tempIt.next().getName()) <= 0){
+							System.out.println("Title");
+							libIt.previous();
+							tempIt.previous();
+							temp.add(tempIt.nextIndex(), libIt.next());	
+							insert = true;
+						}
+						else{
+							libIt.previous();
+						}
+						break;
+					}
+					
+					case artist:{
+						if(libIt.next().getArtist().compareTo(tempIt.next().getArtist()) <= 0){
+							System.out.println("Artist");
+							libIt.previous();
+							tempIt.previous();
+							temp.add(tempIt.nextIndex(), libIt.next());	
+							insert = true;
+						}
+						else{
+							libIt.previous();
+						}
+						break;
+					}
+					
+					case genre:{
+						if(libIt.next().getGenre().compareTo(tempIt.next().getGenre()) <= 0){
+							System.out.println("Genre");
+							libIt.previous();
+							tempIt.previous();
+							temp.add(tempIt.nextIndex(), libIt.next());	
+							insert = true;
+						}
+						else{
+							libIt.previous();
+						}
+						break;
+					}
+					
+					case composer:{
+						if(libIt.next().getComposer().compareTo(tempIt.next().getComposer()) <= 0){
+							libIt.previous();
+							tempIt.previous();
+							temp.add(tempIt.nextIndex(), libIt.next());	
+							insert = true;
+						}
+						else{
+							libIt.previous();
+						}
+						break;
+					}
+					
+					case year:{
+						if(libIt.next().getYear().compareTo(tempIt.next().getYear()) <= 0){
+							libIt.previous();
+							tempIt.previous();
+							temp.add(tempIt.nextIndex(), libIt.next());	
+							insert = true;
+						}
+						else{
+							libIt.previous();
+						}
+						break;
+					}
+					
+					case album:{
+						if(libIt.next().getAlbum().compareTo(tempIt.next().getAlbum()) <= 0){
+							libIt.previous();
+							tempIt.previous();
+							temp.add(tempIt.nextIndex(), libIt.next());	
+							insert = true;
+						}
+						else{
+							libIt.previous();
+						}
+						break;
+					}
+					
+					default:{
+						
+					}
+					}
+
+
+				}
+
+				if(!insert){
+					temp.add(libIt.next());
+				}
+			}}
+		Library = temp;
+	}
 }
